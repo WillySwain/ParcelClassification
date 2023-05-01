5659aa5f4146ba26a9fdef7f7d213a30e16b0953
Willy <williamswain361@gmail.com>
2023-05-01 10:44:00 -0400

    Changed back to iFrame

--- frontend/my-app/src/GradioInterface.js
+++ frontend/my-app/src/GradioInterface.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Gradio } from '@gradio/react';
 
 function GradioInterface() {
   return (
-    <Gradio
-      interface="sidebar"
-      src="https://huggingface.co/spaces/WillySwain/parcelgradio"
-      width="100%"
-      height="800px"
-    />
+<iframe
+	src="https://willyswain-parcelgradio.hf.space"
+	frameborder="0"
+	width="850"
+	height="450"
+></iframe>
   );
 }
 
