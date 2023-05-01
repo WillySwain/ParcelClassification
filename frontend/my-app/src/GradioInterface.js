8d8c0cbb8b5ec5ccc8e984b572da67e0d9cf6891
Willy <williamswain361@gmail.com>
2023-05-01 10:30:23 -0400

    Why does commiting take so long

--- frontend/my-app/src/GradioInterface.js
+++ frontend/my-app/src/GradioInterface.js
@@ -3,7 +3,7 @@ import React from 'react';
 function GradioInterface() {
   return (
     <iframe
-      src="http://127.0.0.1:7860"
+      src="https://huggingface.co/spaces/WillySwain/parcelgradio"
       title="Parcel Classification"
       style={{ width: '100%', height: '800px', border: 'none' }}
     />
