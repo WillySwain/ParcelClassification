74b44841dd686c10fe057a5017e46c2a1d14cff9
Willy <williamswain361@gmail.com>
2023-04-30 22:20:05 -0400

    web fix

--- frontend/my-app/src/About.js
+++ frontend/my-app/src/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <div>
       <h1>Results</h1>
-      <img src={table} width="100" height="50" />
+      <img src={table} alt="A table of evaluation metrics" width="100" height="50" />
     </div>
   );
 }
