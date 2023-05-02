8b60e93f2eb0064ffb065a32f8140015118c31dd
helen19 <helen19@vt.edu>
2023-05-02 19:52:30 -0400

    teaser image

--- frontend/my-app/src/Home.js
+++ frontend/my-app/src/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div>
       <h1>Introduction</h1>
-      <img src={teaser} alt="A table of evaluation metrics" width="700" height="400" style={{ alignSelf: 'center' }}/>
+      <img src={teaser} alt="teaser image" width="700" height="400" style={{ alignSelf: 'center' }}/>
       <h2>Problem Description</h2>
       <p>Parcel damage is a common problem that compromises the safety and can often lead to the damage of the contents of a parcel. Approximately 10% of parcels arrive at their destination damaged. Globally this equates to $15 billion in lost revenue. In most cases, the shipping company is liable for a replacement for the damaged item. This is a significant expense for shipping companies that can be mitigated through identification of where and what type of damage occurred. For our capstone project, we will train 4 different models, which are ResNet50, VGG-19, YOLOv5, and VisualBERT, to determine whether or not a package has been damaged, and compare the performance of all models. We anticipate that the best machine learning model can be incorporated into shipping warehouses to record where in its journey a particular package was damaged to identify patterns of unsafe handling along the distribution network.</p>
       <h2>Motivation</h2>
